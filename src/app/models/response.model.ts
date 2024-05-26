88a9656d5580bda1ae5370123318c97a3193a4d0
migue1angel <macf06200@gmail.com>
2024-05-25 23:15:52 -0500

    response model

---
+++ src/app/models/response.model.ts
@@ -0,0 +1,4 @@
+export interface ResponseModel {
+  data:{},
+  message:string
+}
--- src/app/pages/core/sales/sale/sale.component.ts
+++ src/app/pages/core/sales/sale/sale.component.ts
@@ -1,32 +1,56 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {removeColor} from "@angular/cli/src/utilities/color";
+import {CustomerHttpService} from "../../../../http-services/customer-http.service";
+import {CustomerModel} from "../../../../models/customer.model";
+import {ResponseModel} from "../../../../models/response.model";
 
 @Component({
   selector: 'app-sale',
   templateUrl: './sale.component.html',
   styleUrl: './sale.component.scss'
 })
-export class SaleComponent {
+export class SaleComponent implements OnInit{
+  //show modal
   dialogVisible: boolean = false;
   showDialog() {
     this.dialogVisible = true;
   }
-
+  // forms
   customerForm: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
-    this.customerForm= this.buildCustomerForm
-  }
 
+  constructor(private formBuilder: FormBuilder, private customerHttpService:CustomerHttpService ) {
+    this.customerForm = this.buildCustomerForm
+    this.getCustomers()
+  }
+  
+  ngOnInit(){
+    
+  }
 
   get buildCustomerForm():FormGroup{
     return this.formBuilder.group({
-      identification:['', Validators.required],
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      phone: ['', Validators.required],
-      address: ['', Validators.required],
+      identification:[null , Validators.required],
+      name: [null , Validators.required],
+      email: [null , Validators.required],
+      phone: [null , Validators.required],
+      address: [null , Validators.required],
     })
+  }
+
+  onSubmit(){
+    if (this.customerForm.valid){
+      this.createCustomer(this.customerForm.value)
+      this.customerForm.reset()
+    } else{
+      console.log(this.customerForm.errors)
+    }
+  }
+
+  getCustomers(){
+    return this.customerHttpService.findAll().subscribe(response=>console.log(response))
+  }
+  createCustomer(customer:CustomerModel){
+    return this.customerHttpService.create(customer).subscribe(response => console.log(response));
   }
 
 }
