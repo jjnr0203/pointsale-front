fb3a7394c5c7a500dc638224b602c043a8a5b46a
ddpt18 <diegopucuji18@gmail.com>
2024-06-13 01:52:30 -0500

    list

--- src/app/pages/core/admin/product/product-list/product-list.component.ts
+++ src/app/pages/core/admin/product/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductsHttpService } from '../../../../../http-services/products-http.service';
+import { ProductModel } from '../../../../../models/product.model';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.scss'
 })
 export class ProductListComponent {
+
+  products: any = [];
+
+  constructor(private productsHttpService: ProductsHttpService) {
+    this.findAll();
+  }
+
+  findAll(){
+    this.productsHttpService.findAll().subscribe(response => {
+      this.products = response;
+      console.log(this.products);
+    })
+  }
+
+  //createProduct(product: ProductModel){
+    ////this.products = response;
+    //})
+  //}
+
+  findOneProduct(id:string){
+    this.productsHttpService.findOneProduct(id).subscribe(response =>{
+      this.products = response;
+    })
+  }
+
+  //updateProduct(id:string,product: ProductModel){
+    ////console.log(response);
+    //})
+  //}
+
+  deleteProduct(id: string){
+    this.productsHttpService.deleteProduct(id).subscribe(response =>{
+      console.log(response)
+    })
+  }
 
 }
--- src/app/pages/core/admin/supplier/supplier-list/supplier-list.component.ts
+++ src/app/pages/core/admin/supplier/supplier-list/supplier-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SupplierHttpService } from '../../../../../http-services/supplier-http.service';
+import { SupplierModel } from '../../../../../models/supplier.model';
 
 @Component({
   selector: 'app-supplier-list',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class SupplierListComponent {
 
+  suppliers: any = [];
+
+  constructor(private supplierHttpService: SupplierHttpService) {
+    this.findAll();
+  }
+
+  findAll() {
+   this.supplierHttpService.findAll().subscribe(response => {
+    this.suppliers = response;
+    console.log(this.suppliers);
+   })
+  }
+
+  //createSupplier(supplier: SupplierModel){
+    //this.supplierHttpService.createSupplier(supplier).subscribe(response =>{
+      //console.log(response)
+    //})
+  //}
+
+  finOneShupplier(id:string){
+    this.supplierHttpService.finOneShupplier(id).subscribe(response => {
+      this.suppliers = response;
+    })
+  }
+
+  //updateSupplier(id:string, supplier:SupplierModel){
+    //this.supplierHttpService.updateSupplier(id, supplier). subscribe(response => {
+      //console.log(response);
+    //} )
+  //}
+
+  deleteSupplier(id:string){
+    this.supplierHttpService.deleteSupplier(id).subscribe(response => {
+      console.log(response)
+    })
+  }
 }
