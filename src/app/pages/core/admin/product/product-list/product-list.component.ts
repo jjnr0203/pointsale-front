78a81fe4374253716b024b7f9885589b52f660ad
migue1angel <macf06200@gmail.com>
2024-06-18 09:15:27 -0500

    products by shop

--- src/app/pages/core/admin/product/product-list/product-list.component.ts
+++ src/app/pages/core/admin/product/product-list/product-list.component.ts
@@ -1,28 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductsHttpService } from '../../../../../http-services/products-http.service';
 import { ProductModel } from '../../../../../models/product.model';
+import { LoginHttpService } from '../../../../../http-services/login-http.service';
+import { ShopService } from '../../../../../http-services/shop.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   products: any = [];
   filteredProducts: any = [];
   searchTerm: string = '';
   confirmOpened: boolean = false; 
-
+  user: any;
+  curretnShop:any;
   constructor(
-    private productsHttpService: ProductsHttpService
+    // private loginHttpService:LoginHttpService,
+    private productsHttpService: ProductsHttpService,
+    private shopService:ShopService
   ) {
+    // this.user = this.loginHttpService.getUser()
+    this.curretnShop = this.shopService.getShop()
+  }
+  ngOnInit(): void {
+    this.findAll(this.curretnShop.id)
   }
 
-  findAll(){
-    this.productsHttpService.findAll().subscribe(response => {
-      this.products = response;
+  findAll(shopid:string){
+    this.productsHttpService.findByShop(shopid).subscribe(response => {
+      this.products = response.data;
       this.filteredProducts = this.products
-      console.log(this.products);
+      console.log(response);
     })
   }
 
